eb506ddc348be52c48e912b7f39e7f8a5c902ec6
= <=>
2015-04-20 14:03:40 +0300

    backbone-tree-view example

---
+++ backbone-tree-view/data.js
@@ -0,0 +1,65 @@
+var data = [
+    {
+        id:7,
+        title:'No children'
+    },
+    {
+        id:1,
+        title:'Australia',
+        open : false,
+        nodes: [
+            {
+                id: 2,
+                title : 'Sydney'
+            }
+        ]
+    },
+    {
+        id:3,
+        title:'Russia',
+        open : false,
+        nodes: [
+            {
+                id:4,
+                title : 'Moscow',
+                nodes : [
+                    {
+                        id: '41',
+                        title: 'Moscow - 1'
+                    },
+                    {
+                        id: '42',
+                        title: 'Moscow - 2'
+                    }
+                ]
+            },
+            {title: 'aaaa1'},
+            {title: 'aaaa2'},
+            {title: 'aaaa3'},
+            {title: 'aaaa4'},
+            {title: 'aaaa5'},
+            {title: 'aaaa6'},
+            {title: 'aaaa7'},
+            {title: 'aaaa8'},
+            {title: 'aaaa9'},
+            {title: 'aaaa10'},
+            {title: 'aaaa11'},
+            {title: 'aaaa12'},
+            {title: 'aaaa13'},
+            {title: 'aaaa14'},
+            {title: 'aaaa15'},
+            {title: 'aaaa16'},
+            {title: 'aaaa17'}
+        ]
+    },
+    {
+        id:5,
+        title:'USA',
+        nodes: [
+            {
+                id:6,
+                title : 'Washington'
+            }
+        ]
+    },
+];
